f1031d4e626aee04ad263d3ad1ae756d5ce104bf
juliet oti <egwuotijulietchinenye@gmail.com>
2023-02-20 23:00:43 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello juliet</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam dignissimos
+        adipisci culpa reiciendis facere exercitationem commodi obcaecati, eaque
+        sequi laudantium fugit consectetur, molestias eos, beatae iure corporis
+        veritatis. Obcaecati libero nesciunt ipsam voluptates iste ex nihil odio
+        ratione ipsum repellat.
+      </p>
+    </div>
+  );
+}
+
+export default App;
