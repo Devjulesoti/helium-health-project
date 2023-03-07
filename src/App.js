438a54664bdc2f7a2742536888530971feb77b0b
juliet oti <egwuotijulietchinenye@gmail.com>
2023-03-07 12:41:12 +0100

    the about page commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Aboutus from "./Pages/Aboutus";
 
 function App() {
-  return (
-    <div className="App">
-      <p>Hello juliet</p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam dignissimos
-        adipisci culpa reiciendis facere exercitationem commodi obcaecati, eaque
-        sequi laudantium fugit consectetur, molestias eos, beatae iure corporis
-        veritatis. Obcaecati libero nesciunt ipsam voluptates iste ex nihil odio
-        ratione ipsum repellat.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi atque
-        tempora blanditiis nesciunt consequuntur excepturi qui at, quaerat
-        sapiente quae illo dolorum harum ullam tenetur. Optio hic neque minus
-        repudiandae?
-      </p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus sapiente at quod blanditiis itaque quam, cum optio saepe dolores veritatis nisi, quas voluptatem dignissimos aliquam soluta facere odio iste. Vitae, sint necessitatibus deserunt et quisquam libero, qui dicta aperiam placeat, repellendus illum velit id. Quaerat sint voluptatem ratione unde tempore et, a quam neque praesentium deserunt inventore. Modi, nulla obcaecati!<p/>
-    </div>
-  );
+  return <div className="App">
+    <Aboutus/>
+  </div>;
 }
 
 export default App;
---
+++ src/Components/Container.jsx
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+// const Container = () => {
+//   return (
+//     <div className='cc-container'>Container</div>
+//   )
+// }
+
+// export default Container
